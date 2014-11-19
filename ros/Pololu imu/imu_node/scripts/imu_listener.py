440c5587af2a917a40b587524a59459de0f88a53
mfarich <mfarich@aol.com>
2014-11-19 01:16:00 -0500

    Rewrote process part of imu talker for better performance.  Now runs as fast as the pi can handle pretty much.
    Also added picture of node parts in pelican box.

--- ros/Pololu imu/imu_node/scripts/imu_listener.py
+++ ros/Pololu imu/imu_node/scripts/imu_listener.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+#********WoooHooo!!! Imports!!********#
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
+#***********End 'O Imports************#
 
-def callback(imu_msg):
+def callback(imu_msg):	#imu_msgs will become populated with the imu message from the talker
+     #this all commented out for now while we're playing around with what values from the imu go where
 #    rospy.loginfo(rospy.get_caller_id()+
 #	" I heard: [\nang_vel: %f %f %f  \nlin_accel: %f %f %f \norient: %f %f %f]\n", 
 #	imu_msg.angular_velocity_covariance[0], 
@@ -24,9 +27,9 @@ def callback(imu_msg):
     
 def imu_listener():
 
-    rospy.init_node('imu_listener', anonymous=True)
-    rospy.Subscriber("imu_chatter", Imu, callback)
-    rospy.spin()
+    rospy.init_node('imu_listener', anonymous=True)	#declaring listener node
+    rospy.Subscriber("imu_chatter", Imu, callback)	#subsrcibing to imu_chatter topic
+    rospy.spin()	#WooHoo!! spinning 
         
 if __name__ == '__main__':
     imu_listener()
--- ros/Pololu imu/imu_node/scripts/imu_talker.py
+++ ros/Pololu imu/imu_node/scripts/imu_talker.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 
+#********WoooHooo!!! Imports!!********#
 import os
 import subprocess
 
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
+#***********End 'O Imports************#
 
-seq = 0
+#**********Global Variables***********#
+freq = 10	#sensor polling frequency
+seq = 0	#will keep track of the message sequence number
+#*******End 'O Global Variables*******#
+
 
 def imu_talker():
-    global seq
-    imu_pub = rospy.Publisher('imu_chatter', Imu, queue_size=10)
-    rospy.init_node('imu_talker', anonymous=True)
-    r = rospy.Rate(10) # hz
+    global seq	#bringing seq into this scope
+    imu_pub = rospy.Publisher('imu_chatter', Imu, queue_size=10)  #declaring the publisher
+    rospy.init_node('imu_talker', anonymous=True)	#initializing the node
+    global freq	#bringing freq into this scope
+    r = rospy.Rate(freq)	#frequency in hz
 
-    while not rospy.is_shutdown():
-	imu_msg = Imu()
+    while not rospy.is_shutdown():	#pretty much just runs based on the frequency
+	imu_msg = Imu()	#declares the imu message
 
 	try:
-		raw_data_str = subprocess.check_output(["/home/pi/ros_catkin_ws/src/imu_node/scripts/ahrs_raw.sh"])
-		#rospy.loginfo(raw_data_str)
+		#raw_data_str = subprocess.check_output(["/home/pi/ros_catkin_ws/src/imu_node/scripts/ahrs_raw.sh"])
+		raw_data_str = process.stdout.readline()	#reading next line from the imu process' raw data stream
+		rospy.loginfo(raw_data_str)	#printing raw data stream to ros console
 	except subprocess.CalledProcessError as e:
 		rospy.loginfo("error running imu script")
 
-	raw_vals = []
+	raw_vals = []	#array that imu data from raw data stream will be read into
 	for t in raw_data_str.split():
 		try:
 			raw_vals.append(float(t))
@@ -32,8 +40,7 @@ def imu_talker():
 
 		except ValueError: pass
 	
-	if len(raw_vals) >= 9:
-
+	if len(raw_vals) >= 9:	#making sure we got all 9 values
 
 		imu_msg.orientation.x = raw_vals[0]
 		imu_msg.orientation.y = raw_vals[1]
@@ -49,24 +56,23 @@ def imu_talker():
 
 		imu_msg.header.seq = seq
 		time = rospy.get_rostime()
-		imu_msg.header.stamp.secs = time.secs
-		imu_msg.header.stamp.nsecs = time.nsecs 
+		imu_msg.header.stamp.secs = time.secs	#time in seconds
+		imu_msg.header.stamp.nsecs = time.nsecs #time in nanoseconds
 
-		rospy.loginfo(imu_msg)
-		imu_pub.publish(imu_msg)
-		seq += 1
+		#rospy.loginfo(imu_msg)
+		imu_pub.publish(imu_msg)	#publish imu message to the ros topic (imu_chatter)
+		seq += 1	#incrementing sequence number
 	else:
 		rospy.loginfo("error parsing values")
 
-	
-
-
-
-	r.sleep()
+	r.sleep()	#ros goes night night because of frequency stuff
         
 if __name__ == '__main__':
     try:
+	process = subprocess.Popen(["minimu9-ahrs", "--mode", "raw", "-b", "/dev/i2c-1"],
+		stdout=subprocess.PIPE)	#runs an instance of minimu9-ahrs that we will grab the raw data stream from
 	imu_talker()
+
     except rospy.ROSInterruptException: 
 	rospy.loginfo("main except")
 	pass
