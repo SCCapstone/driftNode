ce594e6720326e3bf5f03789982a09538dbc1457
smithzr2 <smithzr2@email.sc.edu>
2014-12-13 12:52:05 -0500

    Changed the wifi node to send a custom message that separates all of the wifi data

--- ros/wifi_node/scripts/wifi_listener.py
+++ ros/wifi_node/scripts/wifi_listener.py
@@ -3,6 +3,7 @@
 #********Imports********#
 import rospy
 from std_msgs.msg import String
+from wifi_node.msg import WifiMsgs
 #***********End 'O Imports************#
 
 def callback(wifi_msg):	#wifi_msgs will become populated with the wifi message from the talker
@@ -13,7 +14,7 @@ def callback(wifi_msg):	#wifi_msgs will become populated with the wifi message f
 def wifi_listener():
 
     rospy.init_node('wifi_listener', anonymous=True)	#declaring listener node
-    rospy.Subscriber("wifi_chatter", String, callback)	#subsrcibing to wifi_chatter topic
+    rospy.Subscriber("wifi_chatter", WifiMsgs, callback)	#subsrcibing to wifi_chatter topic
     rospy.spin()	#keeping the listener alive
         
 if __name__ == '__main__':
